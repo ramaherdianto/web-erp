d532394a55cc274997388f46e4e485dbeb086448
ramaherdianto <ramadhanherdianto35@gmail.com>
2022-08-26 12:13:24 +0700

    create navbar

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <>
+      <div className="heroSection container" id="hero">
+        <p>Hero</p>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { FaBars } from "react-icons/fa";
+
+const NavigationBar = () => {
+  return (
+    <>
+      <nav className="navbar navbar-dark navbar-expand-lg">
+        <div className="container p-3">
+          <a className="navbar-brand" href="/">
+            Mekari
+          </a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <FaBars />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Beranda
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Tentang Kami
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Layanan
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Harga
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Kontak
+                </a>
+              </li>
+            </ul>
+            <div className="btn-navbar">
+              <button className="btn btn-primary" type="submit">
+                Hubungi Kami
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default NavigationBar;
